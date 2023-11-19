0d2c9ca74e92f6ae4b967baadeaec045047fafaa
Hoang Tran <hoang.tran.fin@gmail.com>
2023-11-19 17:13:17 +0200

    Supported addproduct(), removeProduct(), empty() functions

---
+++ client/src/stores/hooks/useCart.js
@@ -0,0 +1,29 @@
+import { create } from "zustand";
+
+export const useCart = create(set => ({
+	cart: JSON.parse(localStorage.getItem("cart")) || [],
+
+	addProduct: product => {
+		set(state => {
+			const i = state.cart.findIndex(item => item.id === product.id);
+			if (i === -1) {
+				return { ...state, cart: [...state.cart, product]};
+			}
+			product.quantity += state.cart[i].quantity;
+			state.cart[i] = product;
+			return { ...state, cart: [...state.cart]};
+		}, true);
+	},
+
+	removeProduct: productId => {
+		set(state => {
+			const i = state.cart.findIndex(item => item.id === productId);
+			if (i === -1) {
+				return state;
+			}
+			return { ...state, cart: state.cart.filter(item => item.id !== productId)};
+		}, true);
+	},
+
+	empty: () => set(state => ({ ...state, cart: [] }))
+}));
