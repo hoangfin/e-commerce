2aed66b392794a2f8c7677c10883c0e5df2e89d1
Hoang Tran <hoang.tran.fin@gmail.com>
2024-03-14 20:10:55 +0200

    Configured resolve.extensions & resolve.alias

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	resolve: {
+		extensions: [".mjs", ".js", ".mts", ".ts", ".d.ts" , ".jsx", ".tsx", ".json"],
+		alias: [{ find: "@src", replacement: path.resolve(__dirname, "src") }]
+	}
+});
