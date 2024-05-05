fd7598211fda36875e3550e8b460d905e2f4c1a0
Hoang Tran <hoang.tran.fin@gmail.com>
2024-05-05 10:35:07 +0300

    Moved const router to dedicated folder.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,12 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
+import { router } from "@src/routes";
 import { ThemeProvider } from "@mui/material";
 import { mirageTheme } from "@src/themes";
 // import { Elements } from "@stripe/react-stripe-js";
 // import { loadStripe } from "@stripe/stripe-js";
 // import { Checkout, OrderHistory, Payment, Product, Search } from "pages";
-import Root from "@src/routes/Root";
-import Home from "@src/routes/Home";
+
 // import "stores";
 
 /* const stripePromise = loadStripe(
@@ -15,32 +15,6 @@ import Home from "@src/routes/Home";
 ); */
 
 const queryClient = new QueryClient();
-
-const router = createBrowserRouter(
-    createRoutesFromElements(
-        <Route path="/" element={<Root />}>
-            <Route index element={<Home />} />
-            {/* <Route path="product/:id" lazy={() => import("@src/routes/Product")}/> */}
-            {/* <Route path="order-history" element={<OrderHistory />} /> */}
-            {/* <Route
-                path="signin"
-                loader={async () => "empty"}
-                lazy={() => import("@src/routes/Root/Signin")}
-            /> */}
-            {/* <Route path="cart" lazy={() => import("@src/routes/Cart")} /> */}
-
-            {/* <Route
-                path="payment"
-                element={
-                    <Elements stripe={stripePromise}>
-                        <Payment />
-                    </Elements>
-                }
-            /> */}
-        </Route>
-    ),
-    { basename: import.meta.env.BASE_URL }
-);
 
 export default function App() {
     return (
