c5c8e9d1cd51711244e257419091e9ef8a82fbca
Hoang Tran <hoang.tran.fin@gmail.com>
2024-05-07 00:16:06 +0300

    Deleted debounce.js

--- client/src/utils/debounce.js
+++
@@ -1,7 +0,0 @@
-export const debounce = (cb, delay = 300) => {
-    let timeoutID;
-    return (...args) => {
-        clearTimeout(timeoutID);
-        timeoutID = setTimeout(() => cb(...args), delay);
-    };
-};
